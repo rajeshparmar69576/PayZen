0a2bc91fbf706abf9a1de14f9d18f3e98342cf27
rajeshparmar69576 <rp69576@gmail.com>
2025-01-03 17:16:51 +0530

    added authMiddleware for User auth

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+
+const AuthMiddleware = async(req,res,next) => {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({
+            success:false,
+            message:"AuthHeader required"
+        });
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try{
+
+        // verify token
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+        req.userId = decoded.userId
+
+        next()
+    }catch(err){
+      console.error("Error while Token verification:", err.message);
+      res.status(403).json({
+      success: false,
+      message: err.message,
+    });
+    }
+
+}
+
+module.exports = {AuthMiddleware}
