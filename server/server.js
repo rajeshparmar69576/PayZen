e6de6b06243af8a7957c454b339e6f12bfdd1fed
rajeshparmar69576 <rp69576@gmail.com>
2025-01-04 17:28:41 +0530

    created routing for accounts

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const connDB = require('./config/db.js')
 const userRouter = require('./routes/user.routes.js')
+const accountRouter = require('./routes/account.routes.js')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -13,6 +14,7 @@ app.use(cors())
 
 // routes
 app.use('/api/v1/user',userRouter)
+app.use('/api/v1/account',accountRouter)
 
 
 app.listen(PORT,async()=>{
