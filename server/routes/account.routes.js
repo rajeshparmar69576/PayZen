454c5319e29bda1473bb8109f71c3c4c94db83c6
rajeshparmar69576 <rp69576@gmail.com>
2025-01-04 17:49:08 +0530

    added route to fetch user balance

--- server/routes/account.routes.js
+++ server/routes/account.routes.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const { Account } = require('../model/account.model.js')
+const { AuthMiddleware } = require('../middleware/auth.middleware.js')
 const router = express.Router()
 
-router.get()
+router.get('/balance',AuthMiddleware,async(req,res)=>{
+    try{
+        const userId = req.userId
+        const userBalance = await Account.findOne({userId})
+
+        res.status(200).json({
+            success:true,
+            message:"User balance fetched successfully",
+            balance:userBalance.balance
+        })
+    } catch(err){
+        console.error("Error while getting user Balance",err.message)
+        res.status(500).json({
+            success:false,
+            error:err.message
+        })
+    }
+})
 
 module.exports = router
