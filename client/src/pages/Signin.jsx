d96e2b3f8464d77184d47acfdc1f64c22392c20a
rajeshparmar69576 <rp69576@gmail.com>
2025-01-10 13:20:38 +0530

    added notification while sigin

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -11,7 +11,32 @@ const Signin = () => {
   const navigate = useNavigate()
   const [userName,setUserName] = useState("")
   const [password,setPassword] = useState("") 
+  const [notification,setNotification] = useState('')
+  const [isError,setIsError] = useState(false)
 
+  const handleSignin = async() =>{
+    try{
+        const API_URL = import.meta.env.VITE_API_URL;
+        const response = await axios.post(`${API_URL}/api/v1/user/signin`,{
+          userName,
+          password
+        })
+
+        // if sign-in successful
+        localStorage.setItem("token",response.data.token)
+        setNotification("Sign-in successful! Redirecting!")
+        setIsError(false)
+
+        setTimeout(()=>{
+          navigate('/home')
+        },1500)
+    }catch(error){
+      setNotification(
+        error.response?.data?.message || "Sign-in failded. Please try again."
+      );
+      setIsError(true)
+    }
+  }
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
@@ -34,21 +59,24 @@ const Signin = () => {
             }}
            />
           <div className="pt-4">
-            <ButtonComponent onClick={async()=>{
-              const API_URL = import.meta.env.VITE_API_URL;
-              const response = await axios.post(`${API_URL}/api/v1/user/signin`,{
-                userName,
-                password
-              })
-              localStorage.setItem("token",response.data.token)
-              navigate('/home')
-            }} label="Sign in" />
+            <ButtonComponent onClick={handleSignin} label="Sign in" />
           </div>
           <BottomWarning
             to={"/signup"}
             label="Don't have an account?"
             toText="Sign up"
           />
+
+          {/* Notification section */}
+          {notification && (
+            <div 
+              className={`mt-4 p-3 rounded-lg text-center ${
+                isError ? "bg-red-100 text-red-600" : "bg-green-100 text-green-600"
+              }`}
+            >
+              {notification}
+            </div>
+          )}
         </div>
       </div>
     </div>
